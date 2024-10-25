2dc5d5334c53ae918e1b0dc0f64da431459bf00f
Marek Urbaniak <77576495+MRK1309@users.noreply.github.com>
2024-10-25 23:24:05 +0200

    sky

--- gltfLoader/main.js
+++ gltfLoader/main.js
@@ -2,11 +2,24 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
+import { Sky } from 'three/examples/jsm/objects/Sky';
+import { MathUtils } from 'three';
+import { Vector3 } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//Sky
+const sky = new Sky();
+sky.scale.setScalar( 450000 );
 
+const phi = MathUtils.degToRad( 90 );
+const theta = MathUtils.degToRad( 180 );
+const sunPosition = new Vector3().setFromSphericalCoords( 1, phi, theta );
+
+sky.material.uniforms.sunPosition.value = sunPosition;
+
+scene.add( sky );
+//
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -36,8 +49,8 @@ camera.position.z = 5;
 const controls = new  OrbitControls(camera, renderer.domElement);
 
 function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
+  // requestAnimationFrame(animate);
+  // controls.update();
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
