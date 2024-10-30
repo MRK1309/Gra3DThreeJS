506290e0a842e690a6005302c83169a599ae3545
Marek Urbaniak <77576495+MRK1309@users.noreply.github.com>
2024-10-30 17:35:35 +0100

    Pociski

--- gltfLoader/main.js
+++ gltfLoader/main.js
@@ -1,50 +1,85 @@
-import './style.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { createSky } from './sky';
+import { fireProjectile } from './projectile';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//Przygotowanie sceny
+const scene = new THREE.Scene(); 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
+const renderer = new THREE.WebGLRenderer(); 
 
-//Sky
-const sky = createSky();
-scene.add(sky);
+renderer.setSize(window.innerWidth, window.innerHeight); 
+renderer.setAnimationLoop(animate); 
+document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const gridHelper = new THREE.GridHelper(100,100);
+const gridHelper = new THREE.GridHelper(100, 100);
 scene.add(gridHelper);
 
-//GLTFLoader
+//GLTFLoader - załadowanie modelu samolotu
+const modelContainer = new THREE.Object3D();
+let myModel = new THREE.Object3D();
 const loader = new GLTFLoader();
-loader.load('/samolot.glb',function(gltf){
-  scene.add(gltf.scene);
+loader.load('/samolot.glb', function(gltf) {
+    myModel = gltf.scene;
+    modelContainer.add(myModel);
 });
+scene.add(modelContainer);
+
+//Sterowanie i renderowanie
+camera.position.z = modelContainer.position.z + 13;
+camera.position.y = 5;
+const controls = new PointerLockControls(modelContainer, renderer.domElement);
+document.body.addEventListener('click', function () { controls.lock(); }, false);
+modelContainer.attach(camera);
+
+//Pociski
+const projectiles = [];
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Space") {
+        fireProjectile(scene, modelContainer, projectiles);
+    }
+}, false);
+
+function animate() { 
+    controls.moveForward(0.03);
+
+    for (let i = projectiles.length - 1; i >= 0; i--) {
+        const projectile = projectiles[i];
+        
+        projectile.position.add(projectile.userData.velocity);
+
+        if (projectile.position.distanceTo(modelContainer.position) > 100) {
+            scene.remove(projectile);
+            projectiles.splice(i, 1);
+        }
+    }
+
+    renderer.render(scene, camera); 
+}
+
+//Latanie
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        controls.moveForward(0.25);
+    } 
+}
+
+//Niebo i światło
+const sky = createSky();
+scene.add(sky);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 100);
 scene.add(ambientLight);
 
-const spotLight = new THREE.SpotLight(0xffffff,30);
+const spotLight = new THREE.SpotLight(0xffffff, 30);
 spotLight.position.set(0, 15, 5);
-spotLight.target.position.set(0,0,0);
+spotLight.target.position.set(0, 0, 0);
 scene.add(spotLight);
 scene.add(spotLight.target);
 
 const spotLightHelper = new THREE.SpotLightHelper(spotLight);
 scene.add(spotLightHelper);
-
-camera.position.z = 20;
-camera.position.y = 10;
-camera.position.x = -10;
-
-const controls = new  OrbitControls(camera, renderer.domElement);
-
-function animate() {
-  // requestAnimationFrame(animate);
-  // controls.update();
-	renderer.render( scene, camera );
-}
-renderer.setAnimationLoop( animate );
