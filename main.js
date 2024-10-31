eb4a8b7d81277d559c678d76279ec80b60fc3b11
Paweł Tylek <138695112+pawtyl@users.noreply.github.com>
2024-10-31 19:11:55 +0100

    obracanie i niszczenie
    Jeszcze trochę zmodyfikowałem strzelanie, bo zauważyłem, że gdy gra jest spauzowana, to dalej można było strzelać, ale trochę teraz za dużo tych pocisków naraz wystrzela, więc trzeba będzie się zdecydować xd

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ const controls = new PointerLockControls(modelContainer, renderer.domElement);
 modelContainer.attach(camera);
 
 // Obsługa zdarzeń
+controls.addEventListener('change', () => {
+    const euler = new THREE.Euler(0, controls.object.rotation.y, 0, 'YXZ');
+    controls.object.rotation.copy(euler);});
 document.body.addEventListener('click', function (){
     instructions.style.display = 'none';
 	blocker.style.display = 'none';
@@ -29,25 +32,31 @@ const instructions = document.getElementById( 'instructions' );
 document.addEventListener("keydown", onDocumentKeyDown, false);
 document.addEventListener("keyup", onDocumentKeyUp, false);
 
-// GLTFLoader - załadowanie modelu samolotu
+// GLTFLoader - załadowanie modeli
 const loader = new GLTFLoader();
 loader.load('/samolot.glb', function(gltf) {
     modelContainer.add(gltf.scene);
 });
 scene.add(modelContainer);
 
+const opponentModelContainer = new THREE.Object3D();
+loader.load('/samolot.glb', function(gltf) {
+    opponentModelContainer.add(gltf.scene);
+    opponentModelContainer.position.z = modelContainer.position.z - 50;
+    opponentModelContainer.rotation.y = Math.PI;
+});
+scene.add(opponentModelContainer);
+
 // Zmienne
 const projectiles = [];
 const speed = 0.125; 
-let forward = false; let right = false; let left = false;
+let forward = false; let right = false; let left = false; let fire = false;
 const cooldownTime = 5000; let lastTurnTime = 0; 
+let opponentHitCount = 0;
 
 // Główna pętla animacji
 function animate() {
     if (controls.isLocked) {
-        modelContainer.rotation.x = 0 // zakomentuj aby 
-        modelContainer.rotation.z = 0 // móc latać normalnie
-
         controls.moveForward(speed/2);
         if (forward) {
             controls.moveForward(speed);
@@ -55,17 +64,31 @@ function animate() {
 
         if (right){
             controls.moveRight(speed*2);
-            // modelContainer.rotateY(-speed/2)
         }
 
         if (left) {
             controls.moveRight(-speed*2);
         }
 
+        if(fire){
+            fireProjectile(scene, modelContainer, projectiles);
+        }
+
         for (let i = projectiles.length - 1; i >= 0; i--) {
             const projectile = projectiles[i];
             
             projectile.position.add(projectile.userData.velocity);
+
+            if (projectile.position.distanceTo(opponentModelContainer.position) < 1) {
+                opponentHitCount++;
+                scene.remove(projectile);
+                projectiles.splice(i, 1);
+    
+                if (opponentHitCount >= 10) {
+                    scene.remove(opponentModelContainer);
+                }
+                continue;
+            }
     
             if (projectile.position.distanceTo(modelContainer.position) > 100) {
                 scene.remove(projectile);
@@ -100,7 +123,7 @@ function onDocumentKeyDown(event) {
         }
     }
     if (event.code === "Space") {
-        fireProjectile(scene, modelContainer, projectiles);
+        fire = true
     } 
 }
 
@@ -117,13 +140,16 @@ function onDocumentKeyUp(event) {
         left = false;
         lastTurnTime = currentTime; 
     }
+    if (event.code === "Space") {
+        fire = false;
+    } 
 }
 
 // Niebo i światło
 const sky = createSky();
 scene.add(sky);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 100);
 scene.add(ambientLight);
 
 const spotLight = new THREE.SpotLight(0xffffff, 30);
@@ -135,3 +161,5 @@ scene.add(spotLight.target);
 // Pomocnicza siatka
 const gridHelper = new THREE.GridHelper(100, 100);
 scene.add(gridHelper);
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
