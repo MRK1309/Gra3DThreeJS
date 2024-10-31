8fd07713fe42c9d2537aa9d36b75788f5f90a37a
Paweł Tylek <138695112+pawtyl@users.noreply.github.com>
2024-10-31 14:25:39 +0100

    płynniejsze latanie, jakby unik
    unik taki se, blokada kamery góra/dół, ale działa do kitu

--- main.js
+++ main.js
@@ -1,67 +1,134 @@
 import * as THREE from 'three'; 
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { createSky } from './sky';
+import { fireProjectile } from './projectile';
 
-//Przygotowanie sceny
+// Przygotowanie sceny
 const scene = new THREE.Scene(); 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
 const renderer = new THREE.WebGLRenderer(); 
-renderer.setSize( window.innerWidth, window.innerHeight ); 
-renderer.setAnimationLoop( animate ); 
-document.body.appendChild( renderer.domElement ); 
-
-//GLTFLoader - załadowanie modelu samolotu
+renderer.setSize(window.innerWidth, window.innerHeight); 
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement); 
+camera.position.z = 13; camera.position.y = 5; 
 const modelContainer = new THREE.Object3D();
-let myModel = new THREE.Object3D()
+const controls = new PointerLockControls(modelContainer, renderer.domElement);
+modelContainer.attach(camera);
+
+// Obsługa zdarzeń
+document.body.addEventListener('click', function (){
+    instructions.style.display = 'none';
+	blocker.style.display = 'none';
+    controls.lock(); }, false);
+controls.addEventListener( 'unlock', function () {
+    blocker.style.display = 'block';
+    instructions.style.display = '';});
+const blocker = document.getElementById( 'blocker' );
+const instructions = document.getElementById( 'instructions' );
+document.addEventListener("keydown", onDocumentKeyDown, false);
+document.addEventListener("keyup", onDocumentKeyUp, false);
+
+// GLTFLoader - załadowanie modelu samolotu
 const loader = new GLTFLoader();
-loader.load('/samolot.glb',function(gltf){
-    myModel = gltf.scene
-    modelContainer.add(myModel)
+loader.load('/samolot.glb', function(gltf) {
+    modelContainer.add(gltf.scene);
 });
 scene.add(modelContainer);
 
-//Sterowanie i renderowanie
-camera.position.z = modelContainer.position.z+13; camera.position.y = 5; 
-const controls = new PointerLockControls(modelContainer, renderer.domElement)
-document.body.addEventListener( 'click', function () {controls.lock();}, false );
-modelContainer.attach(camera)
+// Zmienne
+const projectiles = [];
+const speed = 0.125; 
+let forward = false; let right = false; let left = false;
+const cooldownTime = 5000; let lastTurnTime = 0; 
 
-function animate() { 
-    controls.moveForward(0.03)
-    renderer.render( scene, camera ); 
+// Główna pętla animacji
+function animate() {
+    if (controls.isLocked) {
+        modelContainer.rotation.x = 0 // zakomentuj aby 
+        modelContainer.rotation.z = 0 // móc latać normalnie
+
+        controls.moveForward(speed/2);
+        if (forward) {
+            controls.moveForward(speed);
+        }
+
+        if (right){
+            controls.moveRight(speed*2);
+        }
+
+        if (left) {
+            controls.moveRight(-speed*2);
+        }
+
+        for (let i = projectiles.length - 1; i >= 0; i--) {
+            const projectile = projectiles[i];
+            
+            projectile.position.add(projectile.userData.velocity);
+    
+            if (projectile.position.distanceTo(modelContainer.position) > 100) {
+                scene.remove(projectile);
+                projectiles.splice(i, 1);
+            }
+        }
+    }
+
+    renderer.render(scene, camera);
 }
 
-document.addEventListener("keydown", onDocumentKeyDown, false);
+// Kontrola klawiszy
 function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-    if (keyCode == 87) {
-        controls.moveForward(0.25)
-     } 
+    const keyCode = event.which;
+
+    if (keyCode === 87) { // W
+        forward = true;
+    } 
+
+    if (keyCode === 68) { // D
+        const currentTime = Date.now();
+        if (currentTime - lastTurnTime > cooldownTime) {
+            right = true 
+            lastTurnTime = currentTime; 
+        }
+    } 
+    if (keyCode === 65) { // D
+        const currentTime = Date.now();
+        if (currentTime - lastTurnTime > cooldownTime) {
+            left = true 
+            lastTurnTime = currentTime;
+        }
+    }
+    if (event.code === "Space") {
+        fireProjectile(scene, modelContainer, projectiles);
+    } 
 }
 
-//Niebo i światło
+function onDocumentKeyUp(event) {
+    const keyCode = event.which;
+    if (keyCode === 87) { // W
+        forward = false;
+    }
+    if (keyCode === 68) { // D
+        right = false;
+    }
+    if (keyCode === 65) { // A
+        left = false;
+    }
+}
+
+// Niebo i światło
 const sky = createSky();
 scene.add(sky);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 100);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-const spotLight = new THREE.SpotLight(0xffffff,30);
+const spotLight = new THREE.SpotLight(0xffffff, 30);
 spotLight.position.set(0, 15, 5);
-spotLight.target.position.set(0,0,0);
+spotLight.target.position.set(0, 0, 0);
 scene.add(spotLight);
 scene.add(spotLight.target);
 
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
-
-//Testowe obiekty
-const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material ); scene.add( cube ); 
-
-const geometry2 = new THREE.BoxGeometry( 3, 1, 1 ); 
-const material2 = new THREE.MeshBasicMaterial( { color: 0x57ff33 } );
-const cube2 = new THREE.Mesh( geometry2, material2 ); scene.add( cube2 ); 
-cube2.position.y -= 1
+// Pomocnicza siatka
+const gridHelper = new THREE.GridHelper(100, 100);
+scene.add(gridHelper);
